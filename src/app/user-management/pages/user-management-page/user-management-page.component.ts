8a6d2d41401dfaf427b98d364029be852bbd1ecc
Aramayis Galichyan <aramayisgalichyan@gmail.com>
2024-11-24 17:34:57 +0400

    Create a user management page

---
+++ src/app/user-management/pages/user-management-page/user-management-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'um-user-management-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-management-page.component.html',
+  styleUrl: './user-management-page.component.scss',
+})
+export class UserManagementPageComponent {}
