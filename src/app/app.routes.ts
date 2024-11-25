d4be86a3ab6520d0a1fc245fe340f1cc03d45d6b
Aramayis Galichyan <aramayisgalichyan@gmail.com>
2024-11-25 08:05:57 +0400

    Create the first routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'user-management',
+    pathMatch: 'full',
+  },
+  {
+    path: 'user-management',
+    loadChildren: () =>
+      import(
+        './user-management/pages/user-management-page/user-management-page.module'
+      ).then((m) => m.UserManagementPageModule),
+  },
+];
